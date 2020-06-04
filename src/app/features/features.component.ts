5c9bf038d56c00da7905e49974b2fe823719d88c
Jose J. Martinez <josejuan.martinez@taiger.com>
2020-06-04 16:05:37 +0200

    Adds suggested sentences

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -15,6 +15,13 @@ export class FeaturesComponent implements OnInit {
   newNerClass = 'd-none';
   validNer = false;
   invalidNerMessage = 'Min. 5 chars';
+  isEnteringNer = false;
+  nerValue = '';
+  generatedSynSentences = [];
+  generatedAntSentences = [];
+  generatedHypSentences = [];
+  generatedSimSentences = [];
+  position = undefined;
 
   POS = ['CC: coordinating conjunction',
     'CD: cardinal digit (one, two, 1)',
@@ -55,23 +62,64 @@ export class FeaturesComponent implements OnInit {
   constructor() { }
   ngOnInit(): void {
     for (const item of this.POS) {
-      if (item.split(':')[0] === this.token.POS) {
+      if (item.split(':')[0] === this.token.linguistic_features.pos) {
         this.selectedPOS = item;
       }
     }
+    this.generateSentences();
   }
-
+  generateSentences() {
+    this.position = parseInt(this.token.linguistic_features.offset.order_in_sentence, 10);
+    this.generatedSynSentences = [];
+    this.generatedAntSentences = [];
+    this.generatedHypSentences = [];
+    this.generatedSimSentences = [];
+    for (const syn of this.token.linguistic_features.tessaurus.synonyms) {
+      const sentenceTokens = [...this.token.original_sentence_tokens];
+      sentenceTokens[this.position] = syn;
+      this.generatedSynSentences.push(sentenceTokens);
+    }
+    for (const ant of this.token.linguistic_features.tessaurus.antonyms) {
+      const sentenceTokens = [...this.token.original_sentence_tokens];
+      sentenceTokens[this.position] = ant;
+      this.generatedAntSentences.push(sentenceTokens);
+    }
+    for (const hyp of this.token.linguistic_features.tessaurus.hypernyms) {
+      const sentenceTokens = [...this.token.original_sentence_tokens];
+      sentenceTokens[this.position] = hyp;
+      this.generatedHypSentences.push(sentenceTokens);
+    }
+    for (const sim of this.token.statistical_features.similar_words) {
+      const sentenceTokens = [...this.token.original_sentence_tokens];
+      sentenceTokens[this.position] = sim;
+      this.generatedSimSentences.push(sentenceTokens);
+    }
+  }
   showNewNER() {
+    this.isEnteringNer = true;
     this.newNerClass = '';
     this.iconNewNerClass = 'va-tbottom d-none';
+    this.nerValue = '';
+    this.validateNER(this.nerValue);
+  }
+  getClassName(index: number) {
+    if (this.position > index + 5 || this.position < index - 5) {
+      return 'd-none';
+    } else {
+      return index === this.position ? 'marked' : '';
+    }
   }
 
   hideNewNER() {
+    this.isEnteringNer = false;
     this.newNerClass = 'd-none';
+    this.iconNewNerClass = 'va-tbottom';
+    this.nerValue = '';
   }
 
 
   validateNER(newValue: string) {
+    this.nerValue = newValue;
     const pattern = '^[A-Z]+$';
     this.validNer = false;
     if (newValue.length >= 5) {
@@ -93,8 +141,18 @@ export class FeaturesComponent implements OnInit {
   saveNER(newValue: string) {
     this.validateNER(newValue);
     if (this.validNer) {
+      this.token.linguistic_features.tessaurus.hypernyms.push(newValue);
       this.hideNewNER();
     }
   }
 
+  getText(i: number, word: string) {
+    if (i === this.position - 5) {
+      return '... ' + word;
+    } else if (i === this.position + 5) {
+      return word + ' ...';
+    } else {
+      return word + ' ';
+    }
+  }
 }
